c91af76c12543bc42d92ae09f288cb1370335e42
Josh Carson <joshkcarson11@gmail.com>
2024-10-23 12:13:53 -0400

    Added home page and routing

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect
+
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def home_page():
-    return 'Hello, World!'
+def index():
+    user = session.get('user')
+    return render_template('index.html', user=user)
+
+@app.route('/login')
+def login():
+    session['user'] = {'name': 'Joshua Carson', 'email': 'joshua@example.com'}
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
+
+@app.route('/decks')
+def decks():
+    return 'My Decks Page'
+
+@app.route('/profile')
+def profile():
+    return 'My Profile Page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

c4ea3a4de74faa4ac50665d54a1d018844e5bad8
Josh Carson <joshkcarson11@gmail.com>
2025-01-29 13:58:06 -0500

    added routing

--- app.py
+++ app.py
@@ -26,6 +26,24 @@ def index():
     user = session.get('user')
     return render_template('index.html', user=user)
 
+@app.route('/login')
+def login():
+    session['user'] = {'name': 'Joshua Carson', 'email': 'joshua@example.com'}
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
+
+@app.route('/decks')
+def decks():
+    return 'My Decks Page'
+
+@app.route('/profile')
+def profile():
+    return 'My Profile Page'
+
 
 
 # @app.route('/login')
@@ -55,5 +73,5 @@ if __name__ == '__main__':
         except Exception as e:
             print(f"Database connection failed: {e}")
 
-    app.run(debug=True,host='0.0.0.0',port=8080)
+    app.run(debug=True,host='0.0.0.0',port=80)
 

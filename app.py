5ba679df0feb753ec61e8874b2b5e8f1821db998
Josh Carson <joshkcarson11@gmail.com>
2025-01-30 12:56:35 -0500

    removed redundant comments

--- app.py
+++ app.py
@@ -46,30 +46,11 @@ def profile():
     return 'My Profile Page'
 
 
-
-# @app.route('/login')
-# def login():
-#     session['user'] = {'name': 'Joshua Carson', 'email': 'joshua@example.com'}
-#     return redirect('/')
-
-# @app.route('/logout')
-# def logout():
-#     session.pop('user', None)
-#     return redirect('/')
-
-# @app.route('/decks')
-# def decks():
-#     return 'My Decks Page'
-
-# @app.route('/profile')
-# def profile():
-#     return 'My Profile Page'
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Database tables created successfully!")
-        
+
         #db test code
         try:
             db.session.execute(text("SELECT 1"))

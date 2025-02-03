4897e772d309abf4d487877ec8500c036e994c62
Josh Carson <joshkcarson11@gmail.com>
2025-02-03 14:31:29 -0500

    Added zone and format enums and models for decks, cards in decks, and particial implementation for user model.

--- models.py
+++ models.py
@@ -1,6 +1,21 @@
+import enum
 from app import db
-from sqlalchemy import UUID
+from sqlalchemy import UUID, Enum
 
+##ENUMS
+class Zone(enum.Enum):
+    main = "MAIN"
+    side = "SIDE"
+    commander = "COMMANDER"
+
+class Format(enum.Enum):
+    standard = "STANDARD"
+    commander = "COMMANDER"
+    hamilton = "HAMILTON"
+
+
+
+##TABLES
 class CardPrice(db.Model):
     __tablename__ = "card_prices"
 
@@ -8,9 +23,7 @@ class CardPrice(db.Model):
     price_date = db.Column(db.Date, primary_key=True, index = True)
     price = db.Column(db.Numeric(10, 2), nullable=False, index = True)  
 
-    __table_args__ = (db.Index("idx_card_price_date", "card_id", "price_date"),)
-
-    card = db.relationship("Card", backref="prices")
+    ##card = db.relationship("Card", backref="card_prices")##TODO adding relationships may make development more convenient
 
     def __repr__(self):
         return f"<CardPrice {self.card_id} - {self.price_date}: {self.price}>"
@@ -24,11 +37,37 @@ class Card(db.Model):
 
     def __repr__(self):
         return f"<Card {self.name}>"
-    
+
+#models below will largely go unused until account creation is implemented
+
 class Deck(db.Model):
     __tablename__ = "decks"
 
     id = db.Column(UUID(as_uuid=True), primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(UUID(as_uuid=True), primary_key=True)#may need to add UUID generation
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey("users.id"), index = True)
+    format = db.Column(Enum(Format), nullable = False)
+    description = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"<Deck {self.id}>"
+
+class DeckCard(db.Model):
+    __tablename__ = "deck_cards"
+
+    card_id = db.Column(UUID(as_uuid=True), db.ForeignKey("cards.id"), primary_key=True)
+    deck_id = db.Column(UUID(as_uuid=True), db.ForeignKey("decks.id"), primary_key=True, index = True)
+    count = db.Column(db.Integer(), nullable=False)
+    zone = db.Column(Enum(Zone), nullable = False)
+
+    def __repr__(self):
+        return f"<DeckCard {self.deck_id}:{self.card_id}>"
+    
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True) #TODO may need to add UUID generation
+    ##TODO probably need to flesh out the rest of the user stuff, will worry about that after security
+    
+    
 

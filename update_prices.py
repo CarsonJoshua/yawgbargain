2f3b1a62eaea827ad3f18987bce9e5351fee0da1
Josh Carson <joshkcarson11@gmail.com>
2025-02-07 14:51:15 -0500

    added db updating methods

--- update_prices.py
+++ update_prices.py
@@ -18,47 +18,29 @@ def fetch_bulk_price_url():
     raise Exception("Price bulk data not found.")
 
 def update_card_prices():
+    today = date.today()
     """Fetch latest card prices and update the database."""
-    
     response = requests.get(fetch_bulk_price_url(), stream=True)
     if response.status_code != 200:
         raise Exception("Failed to download price data.")
+    
+    for line in response.iter_lines():
+        if line == '[':
+            continue
+        if line == ']':
+            break
+        try:
+            card_info = json.loads(line)
+            """Add card to db if not in db"""
+            if not Card.query.filter_by(id=card_info.get("oracle_id")):
+                db.session.add(Card(id=card_info.get("oracle_id"),name=card_info.get("name")))
+            """Add price to db if not in db"""
+            if not CardPrice.query.filter_by(card_id=card_info.get("oracle_id"), price_date=today).first():
+                db.session.add(CardPrice(card_id=card_info.get("oracle_id"), price_date=today, price=card_info.get("prices").get("usd")))
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+    db.session.commit()
 
-    price_data = response.json()
-
-    for card_info in price_data:
-        if not Card.query.filter_by(id=card_info.get("id")):
-            db.session.add(Card(id=card_info.get("id"),name=card_info.get("name")))
-            db.session.commit()
-        
-
-        return
-
-
-
-        # card_name = card_info.get("name")
-        # price = card_info.get("prices", {}).get("usd")  # Get USD price
-
-        # if card_name and price:
-        #     # Check if card exists in the DB
-        #     card = Card.query.filter_by(name=card_name).first()
-
-        #     if not card:
-        #         # Add new card to the DB
-        #         card = Card(name=card_name)
-        #         db.session.add(card)
-        #         db.session.commit()
-        #         print(f"Added new card: {card_name}")
-
-        #     # Check if a price already exists for today
-        #     today = date.today()
-        #     existing_price = CardPrice.query.filter_by(card_id=card.id, price_date=today).first()
-        #     if not existing_price:
-        #         # Insert price into `card_prices`
-        #         new_price = CardPrice(card_id=card.id, price_date=today, price=price)
-        #         db.session.add(new_price)
-        #         db.session.commit()
-        #         print(f"Added price for {card_name}: ${price}")
 
 if __name__ == "__main__":
     with app.app_context():

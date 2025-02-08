00a3ff3cb50d9b4201900c435854a0c41f60d5e2
Josh Carson <joshkcarson11@gmail.com>
2025-02-08 14:30:17 -0500

    fixed error in run line

--- connect_db.py
+++ connect_db.py
@@ -3,4 +3,4 @@ import configparser, subprocess
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-subprocess.run(f'PGPASSWORD="{config["database"]["password"]}" psql -h {config["database"]["host"]} -U {config["database"]["user"]} -d {config["database"]["dbname"]}')
+subprocess.run(f'PGPASSWORD="{config["database"]["password"]}" psql -h {config["database"]["host"]} -U {config["database"]["user"]} -d {config["database"]["dbname"]}', shell=True)

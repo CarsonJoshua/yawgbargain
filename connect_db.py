a7ae7489175d8d4ed78dfba472ec973f632e34da
Josh Carson <joshkcarson11@gmail.com>
2025-02-08 14:27:38 -0500

    added script for easy db connection

---
+++ connect_db.py
@@ -0,0 +1,6 @@
+import configparser, subprocess
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+subprocess.run(f'PGPASSWORD="{config["database"]["password"]}" psql -h {config["database"]["host"]} -U {config["database"]["user"]} -d {config["database"]["dbname"]}')

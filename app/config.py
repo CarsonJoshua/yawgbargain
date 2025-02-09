297d3f82b72f4818808e5297b4375eae171da4a7
Josh Carson <joshkcarson11@gmail.com>
2025-02-08 20:03:44 -0500

    added config reading for host and port

--- app/config.py
+++ app/config.py
@@ -4,6 +4,13 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 class Config:
+
+    # Flask settings
+    DEBUG = config.get("flask", "debug", fallback=True) 
+    HOST = config.get("flask", "host", fallback="0.0.0.0")
+    PORT = int(config.get("flask", "port", fallback=5000))
+
+    # DB settings
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{config["database"]["user"]}:{config["database"]["password"]}@{config["database"]["host"]}/{config['database']['dbname']}"
     )
--- run.py
+++ run.py
@@ -3,4 +3,8 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        debug=app.config["DEBUG"],
+        host=app.config["HOST"],
+        port=app.config["PORT"]
+    )

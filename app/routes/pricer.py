9428e7b88a03b98d2deb02d5723da445f24c8528
Josh Carson <joshkcarson11@gmail.com>
2025-02-13 00:58:28 -0500

    tweaked query

--- app/routes/pricer.py
+++ app/routes/pricer.py
@@ -17,22 +17,49 @@ def pricer():
         card_names = [line.strip() for line in deck_text.split("\n") if line.strip()]
 
 
+        # lowest_prices = (
+        #     db.session.query(
+        #         db.session.query(
+        #             CardPrice.card_id,
+        #             func.max(CardPrice.price_date)
+        #         )
+        #         .group_by(CardPrice.card_id)
+        #         .join(
+        #             Card,
+        #             CardPrice.card_id == Card.id
+        #         )
+        #         .filter(Card.name.in_(card_names))
+        #         .subquery(),
+        #         func.min(CardPrice.price)
+        #     )
+        #     .group_by(Card.name)
+        #     .all()
+        # )
+
+        
+        latest_price_subquery = (
+            db.session.query(
+                CardPrice.card_id,
+                func.max(CardPrice.price_date).label('latest_price_date')  
+            )
+            .group_by(CardPrice.card_id) 
+            .join(Card, CardPrice.card_id == Card.id) 
+            .filter(Card.name.in_(card_names))  
+            .subquery()
+        )
+
+       
         lowest_prices = (
             db.session.query(
-                db.session.query(
-                    CardPrice.card_id,
-                    func.max(CardPrice.price_date)
-                )
-                .group_by(CardPrice.card_id)
-                .join(
-                    Card,
-                    CardPrice.card_id == Card.id
-                )
-                .filter(Card.name.in_(card_names))
-                .subquery(),
-                func.min(CardPrice.price)
+                Card.name,
+                func.min(CardPrice.price).label('lowest_price')  
             )
-            .group_by(Card.name)
+            .join(Card, CardPrice.card_id == Card.id) 
+            .join(latest_price_subquery, 
+                (CardPrice.card_id == latest_price_subquery.c.card_id) &
+                (CardPrice.price_date == latest_price_subquery.c.latest_price_date) 
+            )
+            .group_by(Card.name)  
             .all()
         )
 

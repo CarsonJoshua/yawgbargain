bfa9e7b4cf2a53bf626eee462c4c6e5d316b3314
Joshua Carson <18175844+CarsonJoshua@users.noreply.github.com>
2025-02-21 19:08:22 -0500

    fixed bp name conflict

--- app/routes/release_notes.py
+++ app/routes/release_notes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-release_notes_bp = Blueprint('index', __name__)
+release_notes_bp = Blueprint('release_notes', __name__)
 
 @release_notes_bp.route('/release_notes')
 def release_notes():

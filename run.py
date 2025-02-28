a82d75d137ea9958a459d98521aa3d124b1054cd
Joshua Carson <18175844+CarsonJoshua@users.noreply.github.com>
2025-02-28 05:07:27 -0500

    run.py now correctly utilized venv

--- run.py
+++ run.py
@@ -2,30 +2,32 @@ import os
 import subprocess
 import platform
 
-
-
 def activate_venv():
-    """Returns the appropriate command to activate the virtual environment based on the OS."""
     if platform.system() == "Windows":
-        return os.path.join("venv", "Scripts", "activate")  # Windows activation script
+        return os.path.join("venv", "Scripts", "activate")
     else:
-        return os.path.join("venv", "bin", "activate")  # Linux/macOS activation script
-    
+        return os.path.join("venv", "bin", "activate")
 
-
-
-if __name__ == "__main__":
-    venv_activation = activate_venv()
-    if platform.system() == "Windows":
-        subprocess.call(f'{venv_activation}', shell=True)
-    else:
-        subprocess.call(f'source {venv_activation}', shell=True, executable='/bin/bash')
+def run_flask_app():
     from app import create_app
     from app.config import Config
     app = create_app()
+
     if Config.DEBUG:
         print("Running in debug mode...")
-        app.run(debug=True)  # Run Flask's built-in server
+        app.run(debug=True) 
     else:
         print("Running in production mode...")
-        subprocess.call(f'gunicorn -w 4 -b 127.0.0.1:5000 "app:create_app()"', shell=True)
+        if platform.system() == "Windows":
+            from waitress import serve
+            serve(app, host="0.0.0.0", port=5000)
+        else:
+            subprocess.call(f'gunicorn -w 4 -b 127.0.0.1:5000 "app:create_app()"', shell=True)
+
+if __name__ == "__main__":
+    venv_activation = activate_venv()
+
+    if platform.system() == "Windows":
+        subprocess.call(f'start cmd /K "{venv_activation} && python -c \"from run import run_flask_app; run_flask_app()\""', shell=True)
+    else:
+        subprocess.call(f'source {venv_activation} && python -c "from run import run_flask_app; run_flask_app()"', shell=True, executable='/bin/bash')

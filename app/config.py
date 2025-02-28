864b3d2b1eeb851ddbcbe0a2ef26fee3f3647820
Joshua Carson <18175844+CarsonJoshua@users.noreply.github.com>
2025-02-28 04:43:27 -0500

    changed config so it can be read when outside venv

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ class Config:
 
     # DB settings
     SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{config["database"]["user"]}:{config["database"]["password"]}@{config["database"]["host"]}/{config['database']['dbname']}"
+        f"postgresql://{config['database']['user']}:{config['database']['password']}@{config['database']['host']}/{config['database']['dbname']}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = config["database"]["SQLALCHEMY_TRACK_MODIFICATIONS"]
     SECRET_KEY = "supersecretkey"
